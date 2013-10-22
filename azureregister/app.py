8540e6cabdc487ed0a129938bb6706b3e3b40a4e
William Mayor <mail@williammayor.co.uk>
2013-10-22 16:50:39 +0100

    duh, html

--- azureregister/app.py
+++ azureregister/app.py
@@ -1,5 +1,5 @@
 def application(environ, start_response):
     status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
+    response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
     return '<html>' + '<br />'.join(environ) + '</html>'
