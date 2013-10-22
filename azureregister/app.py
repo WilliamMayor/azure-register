784bf4846f028e5097b9b3a812bc2b4d1bb33e2f
William Mayor <mail@williammayor.co.uk>
2013-10-22 16:43:26 +0100

    playing around with environment

--- azureregister/app.py
+++ azureregister/app.py
@@ -2,4 +2,8 @@ def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
+    yield environ + '\n'
+    yield dir(environ) + '\n'
+    yield dir(start_response) + '\n'
+    yield start_response + '\n'
     yield 'Hello from Windows Azure Websites\n'
