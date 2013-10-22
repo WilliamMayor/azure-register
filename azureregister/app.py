bfad5495ff8589cd6102249b9f0ae40cf3ea1c6e
William Mayor <mail@williammayor.co.uk>
2013-10-22 16:51:56 +0100

    type

--- azureregister/app.py
+++ azureregister/app.py
@@ -2,4 +2,4 @@ def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
-    return '<html>' + '<br />'.join(['%s: %s' % (k, v,) for k, v in environ.iteritem()]) + '</html>'
+    return '<html>' + '<br />'.join(['%s: %s' % (k, v,) for k, v in environ.iteritems()]) + '</html>'
