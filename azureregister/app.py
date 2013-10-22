61c1979524269fbeb74f9f8d76d3fadba42fe96a
William Mayor <mail@williammayor.co.uk>
2013-10-22 16:44:07 +0100

    again

--- azureregister/app.py
+++ azureregister/app.py
@@ -4,6 +4,4 @@ def application(environ, start_response):
     start_response(status, response_headers)
     yield environ + '\n'
     yield dir(environ) + '\n'
-    yield dir(start_response) + '\n'
-    yield start_response + '\n'
     yield 'Hello from Windows Azure Websites\n'
