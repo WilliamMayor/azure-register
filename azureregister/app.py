aa1458a4f7d79323ffb8198f9413088e20c50dbb
William Mayor <mail@williammayor.co.uk>
2013-10-24 09:37:41 +0100

    Changed to easy templates

--- azureregister/app.py
+++ azureregister/app.py
@@ -1,5 +1,31 @@
+BASE = '''<html>
+    <head><title>{title}</title></head>
+    <body>{body}</body>
+</html>
+'''
+
+ISE = BASE.format(
+    title='Oh Noes! Something Went Wrong!',
+    body=('<h1>500 Internal Server Error</h1>'
+          '<p>Sorry, something went wrong on our end. Whoops.</p>')
+    )
+
+
+def build_page(url, query):
+    if url == '/':
+        return BASE.format(
+            title='Welcome',
+            body='Stuff'
+        )
+
+
 def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/html')]
+    try:
+        html = build_page(environ['PATH_INFO'], environ['QUERY_STRING'])
+    except:
+        status = '500 Internal Server Error'
+        html = ISE
     start_response(status, response_headers)
-    return '<html>' + '<br />'.join(['%s: %s' % (k, v,) for k, v in environ.iteritems()]) + '</html>'
+    return html
